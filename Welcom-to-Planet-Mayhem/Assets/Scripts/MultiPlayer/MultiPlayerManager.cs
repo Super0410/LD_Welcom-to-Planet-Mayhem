d05b5a06269c5d5c464c4df76dc39d8fcfe5de90
DESKTOP-SJD56LO\hasee <970173129@qq.com>
2017-04-22 15:17:20 +0800

    Split Scene

---
+++ Welcom-to-Planet-Mayhem/Assets/Scripts/MultiPlayer/MultiPlayerManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MultiPlayerManager : MonoBehaviour
+{
+	
+	#region Public Members
+
+	public event System.Action<int> OnPlayerJoinEvent;
+
+	public Button btn_PlayerAdder;
+
+	#endregion
+
+	#region Private Members
+
+	int playerCount = 0;
+
+	#endregion
+
+	#region Unity Methods
+
+	void Start ()
+	{
+		btn_PlayerAdder.onClick.AddListener (delegate {
+			OnPlayerJoin ();
+		});
+	}
+
+
+	#endregion
+
+	#region Public Methods
+
+	public void OnPlayerJoin ()
+	{
+		if (playerCount > 4)
+			return;
+
+		playerCount++;
+		if (OnPlayerJoinEvent != null)
+			OnPlayerJoinEvent (playerCount);
+	}
+
+	#endregion
+}
---
+++ Welcom-to-Planet-Mayhem/Assets/Scripts/MultiPlayer/_FORTEST_Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _FORTEST_Movement : MonoBehaviour
+{
+	
+	#region Public Members
+
+	public float speed = 5f;
+
+	#endregion
+
+	#region Private Members
+
+	#endregion
+
+	#region Unity Methods
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+		Vector3 inputDir = new Vector3 (horizontal, 0, vertical);
+			
+		transform.Translate (inputDir * Time.deltaTime * speed);
+	}
+
+	#endregion
+
+	#region Public Methods
+
+	#endregion
+
+	#region Private Methods
+
+	#endregion
+}
